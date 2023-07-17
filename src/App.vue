6e1722f8fb8f298fda7c0bb207de9b88d2175fb0
LB16B <ogawa.yui2000@gmail.com>
2023-07-17 19:06:37 +0900

    必要な要素の書き出し

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+import Food from './components/Food.vue';
 </script>
 
 <template>
-
+    <Food />
 </template>
 
 <style scoped>
---
+++ src/components/Food.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Food</h1>
+    <div class="container">
+        <div class="form-container">
+            <input />
+            <button class="save-button">追加する</button>
+        </div>
+        <div class="food-container">
+            <ul>
+                <li>
+                    <span>中身</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+
+input {
+    margin: 40px auto;
+    width: 300px;
+    text-align: center;
+}
+
+.save-button {
+    width: 100px;
+    margin: 10px auto;
+}
+.container {
+
+}
+
+.form-container {
+    background: aliceblue;
+    width: 700px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+}
+
+.food-container {
+    width: 700px;
+    margin: 0 auto;
+    text-align: center;
+}
+
+
+
+li {
+    list-style: none;
+    background: rgb(241, 214, 234);
+    margin-bottom: 8px;
+}
+</style>
