5110b46ddd273955d555b8fa3f937c626f372dcc
LB16B <ogawa.yui2000@gmail.com>
2023-07-20 21:07:49 +0900

    provide

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import Food from './components/Food.vue';
+import TodoList from './components/TodoList.vue'
+import { provide } from 'vue'
+import { todos, addTodo } from './useTodo'
+
+provide('todos', todos)
+// provide('addTodo', addTodo)
 </script>
 
 <template>
-    <Food />
+  <TodoList />
 </template>
 
-<style scoped>
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
